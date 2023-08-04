b6a7411a26c5939397800fc8197542674fa76b5d
Aleir Sanches <aleir.sanches@hotmail.com>
2023-08-03 23:28:49 -0300

    Atualizando  as Activity Home, Cadastro e Esqueci_Senha

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.helloandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
 // novo comentario
 // novo comentario teste 2
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btLogin).setOnClickListener{
+            startActiviy(Intent(this,HomeActivity::class.java))
+
+        }
+
+        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener{
+            startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+        }
+
+        findViewById<TextView>(R.id.btCadastre_se).setOnClickListener{
+            startActivity(Intent(this,CadastroActivity::class.java))
+        }
+
+
+    }
+
+    private fun startActiviy(intent: Intent) {
+
     }
 }
