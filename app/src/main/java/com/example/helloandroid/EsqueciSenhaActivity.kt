4fd475e87eeb38a3e35e5f271dc7b9963d3f7867
Aleir Sanches <aleir.sanches@hotmail.com>
2023-08-15 00:01:56 -0300

    criando novas classes

--- app/src/main/java/com/example/helloandroid/EsqueciSenhaActivity.kt
+++ app/src/main/java/com/example/helloandroid/EsqueciSenhaActivity.kt
@@ -2,10 +2,30 @@ package com.example.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.helloandroid.domair.EsqueciSenhaService
+import com.example.helloandroid.extensions.alert
 
 class EsqueciSenhaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_esqueci_senha)
+        findViewById<Button>(R.id.btenviar).setOnClickListener {
+            onClickEnviar()
+        }
+    }
+    private fun onClickEnviar(){
+        val tLogin = findViewById<TextView>(R.id.tLogin)
+        val login = tLogin.text.toString()
+        val service = EsqueciSenhaService()
+        val ok: Boolean = service.recuperarSenha(login)
+        if (ok){
+            alert("sua nova senha foi enviada para seu e-mail")
+            finish()
+        }
+        else {
+            alert ("Ocorreu um erro ao recuperar senha")
+        }
     }
 }
---
+++ app/src/main/java/com/example/helloandroid/domair/EsqueciSenhaService.kt
@@ -0,0 +1,11 @@
+package com.example.helloandroid.domair
+
+import android.util.Log
+
+class EsqueciSenhaService {
+    fun recuperarSenha (login :String ): Boolean {
+        Log.d ("empresa ", "Simulando recuperar senha : $login" )
+        return true
+    }
+
+}
